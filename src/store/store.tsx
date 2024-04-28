1aadfa1ff3589bbf61cd260e8c6cabc3c644a1f6
Andrea Alunni Guiducci <andrea.aguiducci@gmail.com>
2024-04-28 16:01:26 +0200

    add: redux

---
+++ src/store/store.tsx
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import errorSlice from "./slices/errorSlice";
+
+export const store = configureStore({
+  reducer: {
+    error: errorSlice,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
